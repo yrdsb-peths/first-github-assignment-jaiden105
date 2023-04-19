70b5e86b17e16b8da2cf89922519367603a7085a
jaiden105 <131167242+jaiden105@users.noreply.github.com>
2023-04-19 08:35:13 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+public class Rectangle {
+  private int width;
+  private int height;
+
+  public Rectangle(int width, int width)
+  {
+      this.width = width;
+      this.height = height;
+  }
+
+  public int getArea()
+  {
+      return width * height;
+  }
+
+  public int getHeight()
+  {
+      return height;
+  }
+
+  public int getWidth()
+  {
+      return width;
+  }
+  
+  
+  
+}
