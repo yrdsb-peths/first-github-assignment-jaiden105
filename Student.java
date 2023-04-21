21ba4a3c402947d0fe9d959015210614fe91ec6c
jaiden105 <131167242+jaiden105@users.noreply.github.com>
2023-04-21 08:49:23 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,21 @@
+public class Student {
+  private String firstName;
+  private String lastName;
+  private int gradeLevel;
+  private int volunteerHours;
+ 
+  public Student(String firstName, String lastName, int gradeLevel, int volunteerHours)
+    this.firstName = firstName;    
+    this.lastName = lastName;  
+    this.gradeLevel = gradeLevel;  
+    this.volunteerHours = volunteerHours;  
+  
+  }
 
+  public String toString() {
+    return firstName + " " + lastName + " is in " + gradeLevel + " and has " + volunteerHours + " volunteer hours";
+    
+  }
+
+  
+}
