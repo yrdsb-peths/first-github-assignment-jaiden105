4c6da3ab23e5522554f03e3fcb1edece23a09d3e
jaiden105 <131167242+jaiden105@users.noreply.github.com>
2023-04-27 09:10:59 -0400

    dog file

---
+++ Dog.java
@@ -0,0 +1,30 @@
+public class Dog {
+  private String name;
+  private int age;
+  
+  public Dog(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  public String getName(){
+    return name;
+    }
+   
+  public int getAge(){
+    return age;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
